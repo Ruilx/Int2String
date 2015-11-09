35d78f8ab3207aba1d3fb6a8a6b9d0f3fcfacb88
Ruilx <799382072@qq.com>
2015-11-09 22:47:32 +0800

    Signed-off-by: Ruilx <799382072@qq.com>

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+const char *int2String(int val, int base){
+	char *string = (char *)malloc(sizeof(char)*32);
+	char *p = string;
+	char *p1 = string;
+	unsigned char flag;
+	if(-val == val){
+//		strcpy(string, "8463847412-");
+//		p = &string[10];
+//		val = 0;
+		return "-2147483648";
+	}
+	flag = val > 0 ? 0 : (val = -val, 1);
+	while(val){
+		*p += (*p = val % base, *p) > 9 ? '7' : '0';
+		val /= base;
+		p++;
+	}
+	if(flag) *(p++) = '-';
+	*p-- = 0;
+	while((p - p1) > 1){
+		*p ^= *p1;
+		*p1 ^= *p;
+		*p ^= *p1;
+		p1++, p--;
+	}
+	return string;
+}
+// Total 45bytes;
+
+int main(int argc, const char *argv[]){
+	printf("%s", int2String(-2147483648, 10));
+}
